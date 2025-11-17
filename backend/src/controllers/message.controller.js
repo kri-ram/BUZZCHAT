2b26ca52eb44c09ff6d4436f1911d984bdea9c14
kri-ram <ramlkrishna72771815198@gmail.com>
2025-11-17 10:12:26 +0530

    Messsage routes optimized

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -35,6 +35,20 @@ export const sendMessage = async (req, res) => {
         const { id: receiverId } = req.params;
         const senderId = req.user._id;
 
+        if (!text && !image) {
+            return res.status(400).json({ message: "Text or image is required." });
+        }
+
+        if (senderId.equals(receiverId)) {
+            return res.status(400).json({ message: "Cannot send messages to yourself." });
+        }
+
+        const receiverExists = await User.exists({ _id: receiverId });
+        if (!receiverExists) {
+            return res.status(404).json({ message: "Receiver not found." });
+        }
+
+
         let imageUrl;
         if (image) {
             const uploadResponse = await cloudinary.uploader.upload(image);
@@ -82,7 +96,7 @@ export const getChatPartners = async (req, res) => {
         res.status(200).json(chatPartners);
 
     } catch (error) {
-      console.log("Error in getChatPartners:",error.message)
-      res.status(500).json({error:"Internal sever error"})
+        console.log("Error in getChatPartners:", error.message)
+        res.status(500).json({ error: "Internal sever error" })
     }
 }
